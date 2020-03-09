ea6f91a31f66185472b2addf1af14209fd8c562f
ycjoyce <yeechiajoyce@gmail.com>
2020-03-09 19:49:53 +0800

    信箱

--- dev/js/memberHeader.js
+++ dev/js/memberHeader.js
@@ -1,13 +1,13 @@
-        //hover會員圖片
-        let memPic= document.querySelector('li.memZone div div.memPic');
-        let memZoneUl= document.querySelector('li.memZone ul.memZone');
-        memPic.addEventListener("click",function(){
-            memZoneUl.classList.toggle("open");
-        })
+//hover會員圖片
+let memImg= document.querySelector('li.memZone div div.memPic');
+let memZoneUl= document.querySelector('li.memZone ul.memZone');
+memImg.addEventListener("click",function(){
+    memZoneUl.classList.toggle("open");
+})
 
 
-        //點擊我的信箱
-        let messageBtn= document.querySelector('nav div ul li div.message');
-        messageBtn.addEventListener("click",function(){
-            location.href= "message.html";
-        });
+//點擊我的信箱
+let messageBtn= document.querySelector('nav div ul li div.message');
+messageBtn.addEventListener("click",function(){
+    location.href= "message.html";
+});
