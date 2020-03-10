b1086eb6a1444aef2b653d826284a89d1af6b326
ycjoyce <yeechiajoyce@gmail.com>
2020-03-10 23:10:17 +0800

    信箱右邊(尚未完成)

--- dev/php/showAllMsg.php
+++ dev/php/showAllMsg.php
@@ -3,9 +3,29 @@ session_start();
 require_once("./connectDB.php");
 
 //找此會員跟此寄件人的所有信件
-$sql= "select * from `message` where sendMemNo='{$_REQUEST["sendMemNo"]}' and getMemNo='{$_SESSION["memNo"]}';";
+$sql= "select * from `message` where `sendMemNo` in ('{$_REQUEST["sendMemNo"]}','{$_SESSION["memNo"]}') and `getMemNo` in ('{$_SESSION["memNo"]}','{$_REQUEST["sendMemNo"]}') order by `msgTime`;";
 
 $msg= $pdo->query($sql);
-echo $msg->rowCount();
-$msgRows= $msg->fetchAll(PDO::FETCH_ASSOC);
+
+$xml='<?xml version="1.0"?>';
+    $xml.= "<messages>";//根元素
+    while($msgRow= $msg->fetch(PDO::FETCH_ASSOC)){
+        $xml.= "<message>";
+            $xml.= "<myMemNo>{$_SESSION["memNo"]}</myMemNo>";
+            $xml.= "<sendMemNo>{$msgRow['sendMemNo']}</sendMemNo>";
+            $xml.= "<getMemNo>{$msgRow['getMemNo']}</getMemNo>";
+            $xml.= "<msgTime>{$msgRow['msgTime']}</msgTime>";
+            $xml.= "<msgText>{$msgRow['msgText']}</msgText>";
+            $xml.= "<msgPic>{$msgRow['msgPic']}</msgPic>";
+                $sql= "select * from `memInfo` where memNo='{$msgRow['sendMemNo']}'";
+                $sendMem= $pdo->query($sql);
+                $sendMemRow= $sendMem->fetch(PDO::FETCH_ASSOC);
+            $xml.= "<sendMemName>{$sendMemRow['memName']}</sendMemName>";
+            $xml.= "<sendMemPic>{$sendMemRow['memPic']}</sendMemPic>";
+        $xml.= "</message>";
+    }
+    $xml.= "</messages>";
+header("Content-Type:text/xml");
+echo $xml;
+// echo $msg->rowCount();
 ?>
