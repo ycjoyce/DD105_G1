c31fbe9705e69f5322c74e63ae78ce7b22c64397
jadycain <jadycain@gmail.com>
2020-03-10 12:06:13 +0800

    募款更新

--- dev/js/donation.js
+++ dev/js/donation.js
@@ -1,3 +1,25 @@
+// tab頁籤
+
+$(function(){
+  $("a.tab").on("click",function(e){
+    e.preventDefault();
+
+    $(this).closest("ul").find("a.tab").removeClass("-on");
+    $(this).addClass("-on");
+    // closest() 往上層找 只要找到符合條件的就停止
+
+    $("div.tab").removeClass("-on");
+    $("div.tab." + $(this).attr("data-target")).addClass("-on");
+
+
+  });
+});
+
+
+
+
+
+// FAQ
 $(document).ready(function () {
     $('.ques').on('click', function(e) {e.preventDefault();
     
