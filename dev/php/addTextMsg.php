b7f729eba9312f945f1b0a41f591e3da5d370196
ycjoyce <yeechiajoyce@gmail.com>
2020-03-12 23:22:55 +0800

    修改登入登出、信箱文字訊息功能

---
+++ dev/php/addTextMsg.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+try{
+    require_once("./connectDB.php");
+
+    $sql= "insert into `message` (sendMemNo, getMemNo, msgTime, msgText, msgPic) values ('{$_SESSION["memNo"]}', :getMemNo, date_format(now(),'%Y.%m.%d  %H:%i'), :msgText, null);";
+    $msg= $pdo->prepare($sql);
+    $msg->bindValue(":getMemNo", $_REQUEST["getMemNo"]);
+    $msg->bindValue(":msgText", $_REQUEST["msgText"]);
+    $msg->execute();
+    
+}catch(PDOException $e){
+    echo "錯誤訊息: ".$e->getMessage()."<br>";
+}
+?>
