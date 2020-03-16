d376b447fb2c5d22dd734631df2359e65acca60f
ycjoyce <yeechiajoyce@gmail.com>
2020-03-16 17:21:06 +0800

    信箱特殊字元

--- dest/php/addTextMsg.php
+++ dest/php/addTextMsg.php
@@ -6,9 +6,13 @@ try{
     $sql= "insert into `message` (sendMemNo, getMemNo, msgTime, msgText, msgPic) values ('{$_SESSION["memNo"]}', :getMemNo, date_format(now(),'%Y.%m.%d  %H:%i'), :msgText, null);";
     $msg= $pdo->prepare($sql);
     $msg->bindValue(":getMemNo", $_REQUEST["getMemNo"]);
-    $msg->bindValue(":msgText", $_REQUEST["msgText"]);
-    $msg->execute();
-    
+
+
+    $msg->bindValue(":msgText", htmlspecialchars($_REQUEST["msgText"]));
+
+    echo "特殊字元".htmlspecialchars(str_replace("/&","&amp;",$_REQUEST["msgText"]));
+    // $msg->execute();
+
 }catch(PDOException $e){
     echo "錯誤訊息: ".$e->getMessage()."<br>";
 }
