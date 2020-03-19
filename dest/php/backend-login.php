34ef21ab5a91db92475f3c6521f6662414e637ca
s954716 <s954716@gmail.com>
2020-03-19 17:35:12 +0800

    登入驗證導向後台修改

--- dest/php/backend-login.php
+++ dest/php/backend-login.php
@@ -19,21 +19,17 @@ $manager->execute();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>後台登入</title>
 </head>
 <body>
     
 <?php 
 if($manager->rowCount()==0){
-<<<<<<< HEAD
 	echo "帳密錯誤, <a href='../backend-login.html'>請重新登入</a>";
-=======
-	echo "帳密錯誤, <a href='backend-login.html'>請重新登入</a>";
->>>>>>> fe7fb75b75a3c8748cd395e4c925dc0079879aa1
 }else{
 	$managerRow =$manager->fetch(PDO::FETCH_ASSOC);
-
-	echo $managerRow["managerName"], " 您好~";	
+	
+	header("Location: ../adminManage.html");
 }
 
 ?>
