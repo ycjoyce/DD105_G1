1e876adc5eeeb92c644f04b4bbf7b6f535193953
ycjoyce <yeechiajoyce@gmail.com>
2020-04-01 09:52:22 +0800

    fix index donation

--- dest/js/donation_carousel.js
+++ dest/js/donation_carousel.js
@@ -4,6 +4,7 @@ let nextBtn= document.querySelector('div.carousel_donation div.changePage button
 let carouselArea= document.querySelector('section.donation div.rightSide div.carousel_donation div.card');
 let changeArea= document.querySelector('section.donation div.rightSide div.carousel_donation div.changePage');
 let timerId;
+var leftCards= document.querySelectorAll('section.donation div.leftSide div.card');
 
 function dogAniSize(){
     let dogAnimate= document.querySelector('section.donation div.dogAnimate');
@@ -11,6 +12,44 @@ function dogAniSize(){
     dogAnimate.style.height= upImg.offsetHeight + "px";
 }
 window.addEventListener("resize",dogAniSize);
+
+
+function leftSideCards(){
+    $.ajax({
+        url:"./php/getDonationCard.php",
+        type:"GET",
+        success(data){
+            console.log(data);
+            var content= JSON.parse(data);
+            for(let i=0; i<leftCards.length; i++){
+                //project number
+                leftCards[i].querySelector("div.text span:first-child").innerText=`PROJECT ${content[i].fundNo}`;
+                //title
+                leftCards[i].querySelector("div.text h4 a").innerText=content[i].fundTitle;
+                //date
+                leftCards[i].querySelector("div.text span.date").innerText=content[i].fundStartDate;
+                //目前金額
+                leftCards[i].querySelector("div.amount span.now").innerText=`目前金額${content[i].fundNowAmount}`;
+                leftCards[i].querySelector("input[name='cur']").value=`${content[i].fundNowAmount}`;
+                //目標金額
+                leftCards[i].querySelector("div.amount a.goal").innerText=`目標金額${content[i].fundGoal}`;
+                leftCards[i].querySelector("input[name='goal']").value=`${content[i].fundGoal}`;
+                //圖片
+                leftCards[i].querySelector("div.img img").src=`./img/donation/projectImg/${content[i].fundImg}`;
+                //a連結
+                var href= leftCards[i].querySelectorAll("a");
+                for(var j=0; j<href.length; j++){
+                    href[j].href=`./showproject.php?fundNo=${content[i].fundNo}`;
+                }
+            }
+        },
+        error(data){
+            alert(data);
+        },
+    });
+}
+leftSideCards();
+
 
 
 function donationCarousel(){
@@ -23,12 +62,21 @@ function donationCarousel(){
     var changeNum= document.querySelector('div.carousel_donation div.changePage span');
     var progressBar= document.querySelector('div.carousel_donation div.progress_bar');
 
-    var leftCards= document.querySelectorAll('section.donation div.leftSide div.card');
+    
     var goalVal= leftCards[now-1].querySelector("div.text input[name='goal']").value;
     var curVal= leftCards[now-1].querySelector("div.text input[name='cur']").value;
     var comVal= Math.floor(curVal/goalVal*100)+"%";
     var wave= document.getElementById("wave");
-    wave.style.bottom= Math.floor(curVal/goalVal*100)*0.8-80 +"%";
+    if(Math.floor(curVal/goalVal*100)>100){
+        wave.style.bottom="0%";
+        wave.style.height="110%";
+        wave.style.width="120%";
+    }else{
+        wave.style.bottom= Math.floor(curVal/goalVal*100)*0.8-80 +"%";
+        wave.style.height="100%";
+        wave.style.width="100%";
+    }
+    
 
     projectNum.innerText= leftCards[now-1].querySelector('div.text span:first-child').innerText;
     date.innerText= leftCards[now-1].querySelector('div.text span.date').innerText;
@@ -56,7 +104,7 @@ function donationCarousel(){
         }
     }
     //換大圖
-    bigImg.src=`img/donation_card_${now}.jpg`;
+    bigImg.src=`./img/donation/projectImg/${now}_fundImg.jpg`;
 }
 
 donationCarousel();
